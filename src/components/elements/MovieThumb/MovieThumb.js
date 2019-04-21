addaf24b67d629f1aed3cbe1edcb4f7dd008676e
EdwardTran <tranphu2495@gmail.com>
2019-04-21 12:41:08 -0700

    first commit

---
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './MovieThumb.css';
+
+const MovieThumb = ({ image, movieId, movieName, clickable }) => (
+  <div className="rmdb-moviethumb">
+    {/* You can send props via the Links "to" object. Here we create our own "movieName" */}
+    
+      <Link to={{ pathname: `/${movieId}`,  movieName: `${movieName}`}}> 
+        <img className="clickable" src={image} alt="moviethumb" />
+      </Link>
+    
+  </div>
+)
+
+MovieThumb.propTypes = {
+  image: PropTypes.string,
+  movieId: PropTypes.number,
+  movieName: PropTypes.string,
+  clickable: PropTypes.bool
+}
+
+export default MovieThumb;
